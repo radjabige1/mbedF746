c9573e9b298ceadfbfa6214f532c107c0287c761
radjabige1 <rayane.adjabi@universite-paris-saclay.fr>
2024-05-31 11:06:30 +0200

    code_buzzer

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,55 @@
 #include <mbed.h>
 #include <threadLvgl.h>
+#include <InterruptIn.h>
 #include "demos/lv_demos.h"
 #include <cstdio>
 
 ThreadLvgl threadLvgl(30);
 
+// Définition des objets pour la LED et le bouton
+DigitalOut led1(LED1);
+DigitalIn button(PI_11);
+DigitalOut buzzer(D10);
+
+bool button_was_pressed = false;
+
+
+// Boucle principale
 int main() {
+    // Configure le bouton en mode PullUp (le bouton est actif bas)
+    button.mode(PullUp);
 
-    threadLvgl.lock();
+    while (true) {
 
-    lv_demo_widgets();
+        while (true) {
+        // Vérifie si le bouton est pressé (état bas)
+            if (button == 0) {
+            // Si le bouton était auparavant relâché, il vient d'être pressé
+                if (!button_was_pressed) {
+                // Inverse l'état de la LED
+                led1 = !led1;
 
-    threadLvgl.unlock();
+                if (led1)
+                {
+                    buzzer = 1;
+                }
+                else{
+                    buzzer = 0;
+                }
 
-    while (1) {
-        // put your main code here, to run repeatedly:
-        ThisThread::sleep_for(10ms);
+                // Marque le bouton comme pressé
+                button_was_pressed = true;
+                }
+            }
+
+            // Vérifie si le bouton est relâché (état haut)
+            if (button == 1) {
+                // Réinitialise l'état du bouton
+                button_was_pressed = false;
+            }
+
+            // Attente pour décharger le processeur (50 ms)
+            ThisThread::sleep_for(50ms);
+        }
     }
 }
